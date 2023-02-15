224bd0856252f54b0850539d603a3495426621da
Izabela <izabelaszeremeta@gmail.com>
2023-02-15 14:12:33 -0600

    complete part 2

---
+++ controllers/dogs.js
@@ -0,0 +1,48 @@
+const { Dog } = require("../models")
+
+const create = async (req, res) => {
+  try {
+    const dog = await Dog.create(req.body)
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const index = async (req, res) => {
+  try {
+    const dogs = await Dog.findAll()
+    res.status(200).json(dogs)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const dog = await Dog.findByPk(req.params.id)
+    dog.set(req.body)
+    await dog.save()
+      
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteDog = async (req, res) => {
+  try {
+    const dog = await Dog.findByPk(req.params.id)
+    await dog.destroy()
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+module.exports = {
+  create,
+  index,
+  update,
+  delete: deleteDog,
+}
